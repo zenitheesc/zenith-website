98722443f4325751265e7e2b3a2ff74cdecb2fe0
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-02 19:16:55 -0300

    criando arquivos de cada página

---
+++ pages/Zenith.js
@@ -0,0 +1,16 @@
+import NavBar from '../components/navBar';
+import Footer from '../components/footer';
+
+function Zenith() {
+
+	return (
+		<>
+			<NavBar />
+			Zenith
+			<Footer/>
+		</>
+	)
+
+}
+
+export default Zenith;
