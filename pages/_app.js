1d9d1030c2cab24fdd62b6a8a74885d20101095f
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-02 19:16:06 -0300

    acertando routerização

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/global.css';
 import Home from './index.js';
 
-export default function MyApp() {
-    return <Home />
+export default function MyApp({Component, pageProps}) {
+    return <Component {...pageProps}/>
 }
