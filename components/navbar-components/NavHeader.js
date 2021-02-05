06c0e7041cd8856dc1927418ffd4aa31cf683185
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-05 15:53:27 -0300

    Loógica de abertura do menu e responsividade

---
+++ components/navbar-components/NavHeader.js
@@ -0,0 +1,31 @@
+import style from '../../styles/NavBar.module.css'
+import NavLinks from './NavLinks'
+import React, { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+export default function NavHeader() {
+	const [menu, setMenuState] = useState(true);
+
+	const openMenu = (open) => {
+		setMenuState(open);
+	}
+
+	return (
+		<>
+			<div className={style.navBarContainer}>
+				<a href="./">
+					<img className={style.navBarIcon} src="/images/NavigationBar/logo.png" />
+				</a>
+
+				<NavLinks className={style.navigationHeader} />
+
+				{menu ?
+					<FaTimes className={style.menuIcon} onClick={() => { openMenu(false) }} /> :
+					<FaBars className={style.menuIcon} onClick={() => { openMenu(true) }} />
+				}
+
+
+			</div>
+		</>
+	)
+}
