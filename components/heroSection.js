32ce51d001e6f150370223ec04bb1a6e77c7e106
mairacanal <maira.canal@usp.br>
2021-02-05 10:14:00 -0300

    homepage: Changing m4v video encoder

--- components/heroSection.js
+++ components/heroSection.js
@@ -21,7 +21,7 @@ function HeroSection() {
     return (
       <div className={styles.heroContainer}>
         <video autoPlay loop muted playsInline preload="auto">
-          <source src="images/HomePage/comp5.m4v" type="video/mp4" />
+          <source src="images/HomePage/comp5.m4v"/>
         </video>
         <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
         <hr />
@@ -31,22 +31,23 @@ function HeroSection() {
         </p>
       </div>
     );
+  } else {
+      return (
+        <div className={styles.heroContainer}>
+          <video autoPlay loop muted playsInline preload="auto">
+            <source src="images/HomePage/original.mp4" type="video/mp4" />
+            <source src="images/HomePage/video.webm" type="video/webm" />
+            <source src="images/HomePage/video.ogg" type="video/ogg" />
+          </video>
+          <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
+          <hr />
+          <p>
+            Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade
+            de conquistar o espaço
+          </p>
+        </div>
+      );
   }
-  return (
-    <div className={styles.heroContainer}>
-      <video autoPlay loop muted playsInline preload="auto">
-        <source src="images/HomePage/original.mp4" type="video/mp4" />
-        <source src="images/HomePage/video.webm" type="video/webm" />
-        <source src="images/HomePage/video.ogg" type="video/ogg" />
-      </video>
-      <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
-      <hr />
-      <p>
-        Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade
-        de conquistar o espaço
-      </p>
-    </div>
-  );
 }
 
 export default HeroSection;
