d83082f9eb83704f1de1f5f2958eea415116bb72
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-05 15:53:40 -0300

    Menu mobile

---
+++ components/navbar-components/NavMenu.js
@@ -0,0 +1,10 @@
+import NavLinks from "./NavLinks";
+import style from '../../styles/NavBar.module.css'
+
+export default function NavMenu() {
+	return (
+		<>
+			<NavLinks className={style.navigationMenu}/>
+		</>
+	)
+}
