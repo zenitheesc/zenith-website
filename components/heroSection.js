b1319472f9b9a65bfc4a3d0832e5d6bebc2ab20f
mairacanal <maira.canal@usp.br>
2021-02-04 21:44:45 -0300

    homepage: Adding another video format

--- components/heroSection.js
+++ components/heroSection.js
@@ -5,7 +5,7 @@ function HeroSection() {
     return (
         <div className={styles.heroContainer}>
             <video autoPlay loop muted playsInline preload="auto"> 
-                <source src='images/HomePage/video.mp4' type='video/mp4'/>            
+                <source src='images/HomePage/original.mp4' type='video/mp4'/>            
                 <source src='images/HomePage/video.webm' type='video/webm'/>            
                 <source src='images/HomePage/video.ogg' type='video/ogg'/>            
             </video>
