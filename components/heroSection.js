ea3dc317752b5180d7e7693ab576530c98a97b32
mairacanal <maira.canal@usp.br>
2021-02-05 11:11:44 -0300

    homepage: Changing strategy to adapt to iPhones

--- components/heroSection.js
+++ components/heroSection.js
@@ -1,43 +1,13 @@
 import React from 'react';
-import { isMobileSafari, isAndroid, isMobileOnly } from 'react-device-detect';
 import styles from '../styles/HeroSection.module.css';
 
 function HeroSection() {
-  if (isAndroid) {
-    return (
-      <div className={styles.heroContainer}>
-        <video autoPlay loop muted>
-          <source src="images/HomePage/comp5.m4v"/>
-        </video>
-        <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
-        <hr />
-        <p>
-          Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade
-          de conquistar o espaço
-        </p>
-      </div>
-    );
-  } else if (isMobileSafari) {
     return (
       <div className={styles.heroContainer}>
         <video autoPlay loop muted playsInline preload="auto">
-          <source src="images/HomePage/videoiOS.mp4" type="video/mp4" />
-        </video>
-        <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
-        <hr />
-        <p>
-          Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade
-          de conquistar o espaço
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div className={styles.heroContainer}>
-        <video autoPlay loop muted playsInline preload="auto">
-          <source src="images/HomePage/original.mp4" type="video/mp4" />
+          <source src="images/HomePage/videoIOS.mp4" type="video/mp4" />
+          <source src="images/HomePage/video.mp4" type="video/mp4" />
           <source src="images/HomePage/video.webm" type="video/webm" />
-          <source src="images/HomePage/video.ogg" type="video/ogg" />
         </video>
         <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
         <hr />
@@ -47,7 +17,6 @@ function HeroSection() {
         </p>
       </div>
     );
-  } 
 }
 
 export default HeroSection;
