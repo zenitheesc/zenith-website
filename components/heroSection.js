c98a6f0512e6541d5f86321cf3e343ee57177630
mairacanal <maira.canal@usp.br>
2021-02-05 10:06:35 -0300

    homepage: Adding m4v and H.265 video format

--- components/heroSection.js
+++ components/heroSection.js
@@ -1,20 +1,52 @@
-import React from 'react'
-import styles from '../styles/HeroSection.module.css'
+import React from 'react';
+import { isMobileSafari, isAndroid } from 'react-device-detect';
+import styles from '../styles/HeroSection.module.css';
 
 function HeroSection() {
+  if (isMobileSafari) {
     return (
-        <div className={styles.heroContainer}>
-            <video autoPlay loop muted playsInline preload="auto"> 
-                <source src='images/HomePage/original.mp4' type='video/mp4'/>            
-                <source src='images/HomePage/video.webm' type='video/webm'/>            
-                <source src='images/HomePage/video.ogg' type='video/ogg'/>            
-            </video>
-            <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
-            <hr />
-            <p>Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade de conquistar o espaço</p>
-        </div>
-    )
+      <div className={styles.heroContainer}>
+        <video autoPlay loop muted playsInline preload="auto">
+          <source src="images/HomePage/videoiOS.mp4" type="video/mp4" />
+        </video>
+        <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
+        <hr />
+        <p>
+          Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade
+          de conquistar o espaço
+        </p>
+      </div>
+    );
+  } if (isAndroid) {
+    return (
+      <div className={styles.heroContainer}>
+        <video autoPlay loop muted playsInline preload="auto">
+          <source src="images/HomePage/comp5.m4v" type="video/mp4" />
+        </video>
+        <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
+        <hr />
+        <p>
+          Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade
+          de conquistar o espaço
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div className={styles.heroContainer}>
+      <video autoPlay loop muted playsInline preload="auto">
+        <source src="images/HomePage/original.mp4" type="video/mp4" />
+        <source src="images/HomePage/video.webm" type="video/webm" />
+        <source src="images/HomePage/video.ogg" type="video/ogg" />
+      </video>
+      <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
+      <hr />
+      <p>
+        Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade
+        de conquistar o espaço
+      </p>
+    </div>
+  );
 }
 
-export default HeroSection
-
+export default HeroSection;
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Footer from '../components/footer';
 import HeroSection from '../components/heroSection';
 
 function Home() {
-	return (
-		<>
-			<NavBar />
-			<HeroSection />
-			<Footer/>
-		</>
-	)
+  return (
+    <>
+      <NavBar />
+      <HeroSection />
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
