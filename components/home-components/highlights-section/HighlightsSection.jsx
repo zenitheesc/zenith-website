bfe9472aedf63f0589b386a2a7e2f257fa0d542c
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-09 19:21:18 -0300

    Versão inicial dos contadores

--- components/home-components/highlights-section/HighlightsSection.jsx
+++ components/home-components/highlights-section/HighlightsSection.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import style from '../../../styles/HighlightsSection.module.css';
+import CounterText from './CounterText';
 
 export default function HighlightsSection() {
   return (
-    <>
-
-    </>
+    <div className={style.highlightsContainer}>
+      <div>
+        <CounterText value={25} suffix="PROJETOS" />
+        <CounterText value={400} suffix="MEMBROS DESDE FUNDAÇÃO" plus />
+        <CounterText value={2} suffix="CAMPUS PARTY" />
+        <CounterText value={3} suffix="CUBEDESIGNs" />
+        <CounterText value={7} suffix="REPORTAGENS NA MÍDIA" />
+      </div>
+      <div>
+        <h1>
+          Eficiência descrita em números
+        </h1>
+        <h3>
+          Desde 2014
+        </h3>
+      </div>
+    </div>
   );
 }
