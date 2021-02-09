5f7dfb6001a8e65dae3a8f5df98632a94c22425c
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-09 19:21:54 -0300

    Adaptando biblioteca para o estilo a ser utilizado

--- components/home-components/highlights-section/CounterText.jsx
+++ components/home-components/highlights-section/CounterText.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import { countUp } from '../../../styles/HighlightsSection.module.css';
 
-export default function CounterText() {
+export default function CounterText(props) {
   return (
     <>
+      <div className={countUp}>
+        <CountUp prefix={props.plus ? '+' : ''} end={props.value} />
+        <h2>
+          &emsp;
+          {props.suffix}
+        </h2>
+        <br />
+      </div>
 
     </>
   );
