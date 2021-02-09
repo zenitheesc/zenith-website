9edd0fe88401962bd076b6364032bafa686a8863
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-09 20:57:05 -0300

    Ativando animação apenas quando dentro do campo de visão

--- components/home-components/highlights-section/CounterText.jsx
+++ components/home-components/highlights-section/CounterText.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 import { countUp } from '../../../styles/HighlightsSection.module.css';
 
 export default function CounterText(props) {
+  const { value } = props;
+  const { suffix } = props;
+  const { plus } = props;
+  const [isVisible, setIsVisible] = useState(false);
+
+  const startCounting = (isInViewPort) => {
+    if (isInViewPort) {
+      setIsVisible(true);
+    }
+  };
+
   return (
     <>
       <div className={countUp}>
-        <CountUp prefix={props.plus ? '+' : ''} end={props.value} />
+        <CountUp prefix={plus ? '+' : ''} end={value} start={isVisible ? null : 0}>
+          {({ countUpRef }) => (
+            <VisibilitySensor
+              active={!isVisible}
+              onChange={startCounting}
+              delayedCall
+            >
+              <span ref={countUpRef} />
+            </VisibilitySensor>
+          )}
+        </CountUp>
         <h2>
-          {props.suffix}
+          {suffix}
         </h2>
         <br />
       </div>
