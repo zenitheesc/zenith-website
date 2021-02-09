0cd786c3487cb61fd89ce1dc27c2618d486a5d74
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-09 19:22:07 -0300

    Adicionando seção na página inicial

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from '../components/navbar-components/NavBar';
 import Footer from '../components/footer';
 import HomeHeroSection from '../components/heroSection';
+import HighlightsSection from '../components/home-components/highlights-section/HighlightsSection';
 
 function Home() {
   return (
     <>
       <NavBar />
       <HomeHeroSection />
+      <HighlightsSection />
       <Footer />
     </>
   );
