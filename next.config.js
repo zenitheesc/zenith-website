1a66be238938817b9efe3214a8ca47abb92c6325
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-11 23:07:01 -0300

    criando lógica de tradução

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  i18n: {
+    locales: ['pt', 'en', 'es'],
+    defaultLocale: 'es',
+  },
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+
 import NavBar from '../components/navbar-components/NavBar';
 import Footer from '../components/footer';
 import HomeHeroSection from '../components/heroSection';
 import HighlightsSection from '../components/home-components/highlights-section/HighlightsSection';
-import TextContent from '../public/locale/pt-br/homePage.json';
+
+import LanguageSetter from '../components/utils/LanguageSetter';
+
+import TextContentBr from '../public/locale/pt-br/homePage.json';
+import TextContentEn from '../public/locale/en-us/homePage.json';
+import TextContentEs from '../public/locale/es/homePage.json';
 
 function Home() {
+  const TextContent = LanguageSetter(TextContentBr, TextContentEn, TextContentEs);
+
   return (
     <>
       <NavBar textContent={TextContent.navBar} />
