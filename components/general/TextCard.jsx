ba540081b5760a9adc65b25dcbbbcb9cd1825c3d
Leonardo Celente <leocelente@gmail.com>
2021-02-13 12:07:24 -0300

    Esqueleto do Componente

--- components/general/TextCard.jsx
+++ components/general/TextCard.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-export default function TextCard() {
-  return (
-    <>
-
-    </>
-  );
+const defaultText = (
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum optio
+        dignissimos quas esse, id aliquid dolore possimus. Ex, saepe corporis!
+        Eveniet magni veniam, laudantium aut quidem saepe! Vel, dolor
+        perferendis!
+    </p>
+);
+const defaultImg = <img src="http://placekitten.com/200/300" />;
+function TextCard({
+    title = "Title",
+    text = defaultText,
+    img = defaultImg,
+    reverse = false,
+}) {
+    return (
+        //TODO: Achar um jeito melhor de customizar a class css
+        <div className={reverse ? "text-card text-card-reverse" : "text-card"}>
+            <div className="text-card-text">
+                <h2>{title}</h2>
+                {text}
+            </div>
+            <div className="text-card-img">{img}</div>
+        </div>
+    );
 }
+TextCard.propTypes = {
+    title: PropTypes.string,
+    text: PropTypes.node,
+    img: PropTypes.node,
+    reverse: PropTypes.bool,
+};
+export default TextCard;
