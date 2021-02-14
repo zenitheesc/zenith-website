baba25047653fd5c2f8503f71ed1bfeeaf94ed0f
mairacanal <maira.canal@usp.br>
2021-02-13 21:17:25 -0300

    fix: fixing git revert

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,6 @@ import NavBar from '../components/navbar-components/NavBar';
 import Footer from '../components/footer';
 import HomeHeroSection from '../components/heroSection';
 import HighlightsSection from '../components/home-components/highlights-section/HighlightsSection';
-import GenusBanner from '../components/banners-components/GenusBanner'
-import ZenMonitorBanner from '../components/banners-components/ZenMonitorBanner'
-import ApoiadoresBanner from '../components/banners-components/ApoiadoresBanner'
 import TextContent from '../public/locale/pt-br/homePage.json';
 
 function Home() {
@@ -13,10 +10,7 @@ function Home() {
     <>
       <NavBar textContent={TextContent.navBar} />
       <HomeHeroSection />
-      <GenusBanner/>
-		  <ZenMonitorBanner/>
       <HighlightsSection textContent={TextContent.homePageStatistics} />
-      <ApoiadoresBanner/>
       <Footer />
     </>
   );
