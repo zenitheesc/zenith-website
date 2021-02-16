10115e314d74d959c762a8e091e2c7f29c5feb2f
mairacanal <maira.canal@usp.br>
2021-02-16 18:43:54 -0300

    fix: fixing relative paths

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import NavBar from '../components/navbar-components/NavBar';
-import Footer from '../components/Footer';
 import HomeHeroSection from '../components/home-components/HeroSection';
 import HighlightsSection from '../components/home-components/highlights-section/HighlightsSection';
+import Footer from '../components/Footer';
 import TextContent from '../public/locale/pt-br/homePage.json';
 
 function Home() {
--- pages/processo-seletivo.jsx
+++ pages/processo-seletivo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from '../components/navbar-components/NavBar';
-import Footer from '../components/footer';
 import HeroSection from '../components/general/HeroSection';
+import Footer from '../components/Footer';
 import TextContent from '../public/locale/pt-br/homePage.json';
 
 function ProcessoSeletivo() {
