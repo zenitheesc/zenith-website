77b256e6a2ec4e393195d917857f93c27ea8e386
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-17 22:01:53 -0300

    adaptando seção dos destaques ao novo modo de tradução

--- components/home-components/highlights-section/HighlightsSection.jsx
+++ components/home-components/highlights-section/HighlightsSection.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import useTranslation from 'next-translate/useTranslation';
 import style from '../../../styles/HighlightsSection.module.css';
 import CounterText from './CounterText';
 
-export default function HighlightsSection(props) {
-  const { title } = props.textContent;
-  const { subtitle } = props.textContent;
-  const { statistics } = props.textContent;
+export default function HighlightsSection() {
+  const { t } = useTranslation();
+  const title = t('homePage:homePageStatistics.title');
+  const subtitle = t('homePage:homePageStatistics.subtitle');
+  const statistics = t('homePage:homePageStatistics.array', { count: -1 }, { returnObjects: true });
 
   return (
     <div className={style.highlightsContainer}>
       <div className={style.countUpContainer}>
         <CounterText value={statistics[0].number} suffix={statistics[0].description} />
-        <CounterText value={statistics[1].number} suffix={statistics[1].description} plus/>
+        <CounterText value={statistics[1].number} suffix={statistics[1].description} plus />
         <CounterText value={statistics[2].number} suffix={statistics[2].description} />
         <CounterText value={statistics[3].number} suffix={statistics[3].description} />
         <CounterText value={statistics[4].number} suffix={statistics[4].description} />
