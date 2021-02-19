d46ee8739a7bb29d9f47e27c26fe15547d2db832
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-19 20:52:12 -0300

    Simplificando css geral

--- components/projects-components/ProjectData.jsx
+++ components/projects-components/ProjectData.jsx
@@ -4,7 +4,7 @@ import styles from '../../styles/projects/ProjectData.module.css'
 export default function ProjectData(props) {
 	const { dark } = props
 	return (
-		<div className={dark ? styles.projectDataDark : styles.projectData}>
+		<div className={[styles.projectData,dark ? styles.dark : ""].join(" ")}>
 			{props.children}
 		</div>
 	)
