65ef012b8e282fe4300bfab02589062f9815ee74
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-19 20:52:28 -0300

    Removendo impressão desnecessária

--- components/projects-components/projects-structure/ProjectTable.jsx
+++ components/projects-components/projects-structure/ProjectTable.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from '../../../styles/projects/ProjectTable.module.css'
 
 export default function ProjectTable(props) {
-	console.log(props);
 	return (
 		<div className={styles.projectTable}>
 			{props.children}
