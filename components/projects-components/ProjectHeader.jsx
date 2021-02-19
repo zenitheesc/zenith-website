d3b62024ee9b733a53df2d94e70f18414ece1e1e
mairacanal <maira.canal@usp.br>
2021-02-19 16:33:53 -0300

    component: adding project image carousel

--- components/projects-components/ProjectHeader.jsx
+++ components/projects-components/ProjectHeader.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export default function ProjectHeader() {
-  return <></>;
+  const { title } = props.textContent;
+  const { subtitle } = props.textContent;
+
+  return (
+    <section className={styles.projectHeader}>
+      <div className={styles.textSection}>
+        <h1>
+          {title}
+        </h1>
+        <h2>
+          {subtitle}
+        </h2>
+      </div>
+    </section>
+  );
 }
--- components/projects-components/projects/SondasAeroespaciais.jsx
+++ components/projects-components/projects/SondasAeroespaciais.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProjectsCarousel from '../ProjectsCarousel';
 
 export default function SondasAeroespaciais() {
   return (
     <div
       style={{ backgroundColor: '#E1E1E5', width: '100%', height: '60px' }}
-    />
+    > 
+    </div>
   );
 }
