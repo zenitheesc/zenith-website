00206685cee39f5256567d014fe1a0ae01ac2324
Jorge Marcelo Risco <jorgedelano7@gmail.com>
2021-02-20 17:34:10 -0300

    added TechnologiesCloud

--- components/home-components/sectors-section/TechnologiesCloud.jsx
+++ components/home-components/sectors-section/TechnologiesCloud.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import TagCloud from './tagCloud/index';
+// https://www.npmjs.com/package/react3dtagcloud
 
-export default function TechnologiesCloud() {
-  return <></>;
+export default function TechnologiesCloud(props) {
+  const undefinedWarning = ['undefined', 'undefined', 'undefined']
+  const { stringList, speed, radius, url } = props;
+
+  return (
+    <>
+      <div style={{ width: '1000px', height: '1000px', }}>
+        <TagCloud tagName={stringList || undefinedWarning} speed={speed || 10} radius={radius || 100} url={url || '#'} />
+      </div>
+    </>
+  );
 }
