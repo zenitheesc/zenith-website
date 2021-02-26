18b113c0a46e1fb8c96af3d83660db75dcc2af1b
Matheus Vieira <matheus.v4200@gmail.com>
2021-02-26 18:45:00 -0300

    Adaptando seção ao novo contador

--- components/home-components/highlights-section/HighlightsSection.jsx
+++ components/home-components/highlights-section/HighlightsSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import CounterText from './CounterText';
+import CounterText from '../../general/CounterText';
 import style from 'styles/HighlightsSection.module.css';
+import { countUp } from 'styles/HighlightsSection.module.css';
 
 export default function HighlightsSection(props) {
   const { title } = props.textContent;
@@ -11,12 +12,12 @@ export default function HighlightsSection(props) {
     <section className={`-section ${style.section}`}>
       <div className={`-standardContainer ${style.highlightsContainer}`}>
         <div className={style.countUpContainer}>
-          <CounterText value={statistics[0].number} suffix={statistics[0].description} />
-          <CounterText value={statistics[1].number} suffix={statistics[1].description} plus />
-          <CounterText value={statistics[2].number} suffix={statistics[2].description} />
-          <CounterText value={statistics[3].number} suffix={statistics[3].description} />
-          <CounterText value={statistics[4].number} suffix={statistics[4].description} />
-          <CounterText value={statistics[5].number} suffix={statistics[5].description} />
+          <CounterText className={countUp} value={statistics[0].number} suffix={statistics[0].description} />
+          <CounterText className={countUp} value={statistics[1].number} suffix={statistics[1].description} plus />
+          <CounterText className={countUp} value={statistics[2].number} suffix={statistics[2].description} />
+          <CounterText className={countUp} value={statistics[3].number} suffix={statistics[3].description} />
+          <CounterText className={countUp} value={statistics[4].number} suffix={statistics[4].description} />
+          <CounterText className={countUp} value={statistics[5].number} suffix={statistics[5].description} />
         </div>
         <div className={style.textSection}>
           <h1 className="-homepage-section-title">
