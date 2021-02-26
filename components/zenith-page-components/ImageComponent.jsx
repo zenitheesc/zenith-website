c9dd6df546b334690189cb3a7f7c9b447ced3e69
VitorFCM <vitor.carreramiguel@gmail.com>
2021-02-25 23:18:47 -0300

    Images added

--- components/zenith-page-components/ImageComponent.jsx
+++ components/zenith-page-components/ImageComponent.jsx
@@ -3,11 +3,12 @@ import styles from '../../styles/ZenithImageComponent.module.css'
 export default function ImageComponent() {
 
     return (
-        <div className={styles.grid}>
-            
+      <>
+        <div className={styles.imgSection}>
+            <img id={styles.imgOne} src="/images/Zenith/DSC_01371.webp" alt=""/>
+            <img id={styles.imgTwo} src="/images/Zenith/DSC_00911.webp" alt=""/>
         </div>
+        <div className={styles.test}></div>
+        </>
     );
   }
-
-  /*<img src="/images/Zenith/DSC_01371.webp" alt=""/>
-    <img src="/images/Zenith/DSC_00911.webp" alt=""/>*/
