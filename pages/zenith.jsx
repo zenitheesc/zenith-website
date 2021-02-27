fc7b0fb92375329f80e4039fdfe51b36fda99ced
Leonardo Celente <leocelente@gmail.com>
2021-02-27 20:16:07 -0300

    Merge correction

--- pages/zenith.jsx
+++ pages/zenith.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "../components/navbar-components/NavBar";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import HeroSection from "../components/general/HeroSection";
 import TextContent from "../public/locale/pt-br/oZenith.json";
 import Card from "../components/general/Card";
