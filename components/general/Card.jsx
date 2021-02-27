e5e0673f794f4e8109aadc0a862aa51c485167e5
Leonardo Celente <leocelente@gmail.com>
2021-02-27 19:31:26 -0300

    Size customization by props

--- components/general/Card.jsx
+++ components/general/Card.jsx
@@ -5,7 +5,7 @@ import style from "../../styles/TextCard.module.css";
 
 const defaultText = (
     <div>
-        <h2>Titulo</h2>
+        <h1>Titulo</h1>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum optio
             dignissimos quas esse, id aliquid dolore possimus. Ex, saepe
@@ -16,15 +16,15 @@ const defaultText = (
     </div>
 );
 const defaultImg = <img src="http://placekitten.com/960/600" />;
-function Card({
-    left = defaultText,
-    right = defaultImg,
-    lean = "right"
-}) {
+function Card({ left = defaultText, right = defaultImg, lean = "right" }) {
+    let l = lean == "left" ? style.bigger : style.smaller;
+    let r = lean == "left" ? style.smaller : style.bigger;
     return (
         <div className={style.card}>
-            <div className={style.card__left}>{left}</div>
-            <div className={style.card__right}>{right}</div>
+            <div className={`${style.card__left} ${l}`}>{left}</div>
+            <div className={`${style.card__right} ${r}`}>
+                {right}
+            </div>
         </div>
     );
 }
