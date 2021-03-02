5438641109c65292716aaed2f3de354a2b089be2
VitorFCM <vitor.carreramiguel@gmail.com>
2021-03-02 17:50:54 -0300

    Zenith Monitor image in png format added for safari responsiveness

--- components/banners-components/ZenMonitorBanner.jsx
+++ components/banners-components/ZenMonitorBanner.jsx
@@ -6,7 +6,12 @@ function zenMonitorBanner(props) {
 	return (
         <div className={styles.zenMonitorBanner}>
 			<BannersTextSec textContent={props.textContent[1]}/>
-			<img className={styles.zenMonitorImage} src="/images/banners/monitor-banner/ZenithMonitor.webp" />
+			<picture>
+			  <source srcset="/images/banners/monitor-banner/ZenithMonitor.webp" type="image/webp" />
+			  <source srcset="/images/banners/monitor-banner/ZenithMonitor.png" type="image/png" />
+			  <img src="/images/banners/monitor-banner/ZenithMonitor.png" type="image/png" className={styles.zenMonitorImage}/>
+			</picture>
+			
 		</div>
 		
     )
