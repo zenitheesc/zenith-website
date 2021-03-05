7c45bd237e829ba945e95c8376459e30dce50e10
mairacanal <maira.canal@usp.br>
2021-03-05 20:03:20 -0300

    images: adding right format images

--- components/banners-components/ZenMonitorBanner.jsx
+++ components/banners-components/ZenMonitorBanner.jsx
@@ -21,13 +21,11 @@ function zenMonitorBanner(props) {
 			<div className={`-standardContainer ${styles.zenMonitorBanner}`}>
 				{videoAutoPlayable ? 
 					<video className={styles.zenMonitorImage} ref={videoElement} autoPlay muted playsInline preload="auto">
-						<source src="/images/HomePage/banners/monitor-banner/out.mp4" type="video/mp4" />
+						<source src="/images/HomePage/banners/monitor-banner/video.webm" type="video/webm" />
+						<source src="/images/HomePage/banners/monitor-banner/video.mp4" type="video/mp4" />
 					</video>
 					: 
-					<picture>
-						<source srcset="/images/HomePage/banners/genus-banner/Genus.webp" type="image/webp" />
-						<img src="/images/HomePage/banners/genus-banner/Genus.png" type="image/png" id={styles.genusImage}/>
-					</picture>
+					<img src="/images/HomePage/banners/monitor-banner/static.webp" type="image/webp" />
 				}
 				<BannersTextSec textContent={props.textContent[1]}/>
 			</div>
