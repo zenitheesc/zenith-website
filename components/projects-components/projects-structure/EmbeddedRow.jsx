9de8b120b376a509251b66bae501e1ec83b81a66
Matheus Vieira <matheus.v4200@gmail.com>
2021-03-05 21:29:42 -0300

    projects-structure: Aplicando novo parser

--- components/projects-components/projects-structure/EmbeddedRow.jsx
+++ components/projects-components/projects-structure/EmbeddedRow.jsx
@@ -1,17 +1,22 @@
 import Embarcados from 'components/home-components/sectors-section/icons/Embarcados';
 import React from 'react'
 import style from 'styles/projects/SectorsRow.module.css'
+import boldParser from '../../utils/boldParser'
 
 export default function EmbeddedRow(props) {
 	const { content } = props;
+
+	const parsedContent = content.map((string)=>{ return boldParser(string)})
+
 	return (
 		<div className={style.SectorRow}>
 			<div>
 				<Embarcados />
 			</div>
 			<div className={style.SectorRowContent}>
-				{content.map((value, index) => {
-					return <span name={value} key={index}>{value}<br /></span>
+				
+				{parsedContent.map((value, index) => {
+					return <span key={index}>{value}<br /></span>
 				})}
 			</div>
 		</div>
--- components/projects-components/projects-structure/StructureRow.jsx
+++ components/projects-components/projects-structure/StructureRow.jsx
@@ -1,23 +1,20 @@
 import Estruturas from 'components/home-components/sectors-section/icons/Estruturas';
 import React from 'react'
 import style from 'styles/projects/SectorsRow.module.css'
+import boldParser from '../../utils/boldParser'
 
 export default function StructureRow(props) {
 	const { content } = props;
-	content.map(value =>{
-		const replace = ','
-		for (var i = 0; i < value.length; i++) {
-			value[i];
-		  }
-	})
+	const parsedContent = content.map((string)=>{ return boldParser(string)})
+
 	return (
 		<div className={style.SectorRow}>
 			<div>
 				<Estruturas />
 			</div>
 			<div className={style.SectorRowContent}>
-				{content.map((value, index) => {
-					return <span name={value} key={index}>{value}</span>
+				{parsedContent.map((value, index) => {
+					return <span key={index}>{value}</span>
 				})}
 			</div>
 		</div>
