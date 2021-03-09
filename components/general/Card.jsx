a873a278892bf59d927b7d3b23dd0c6d2ce0a43d
Leonardo Celente <leocelente@gmail.com>
2021-03-09 18:09:49 -0300

    Cleanup e mais organizado

--- components/general/Card.jsx
+++ components/general/Card.jsx
@@ -27,15 +27,10 @@ function Card({ left = defaultText, right = defaultImg, lean = "right" }) {
         r = l;
     } else {
     }
-    // let l = lean == "left" ? style.bigger : style.smaller;
-    // let r = lean == "left" ? style.smaller : style.bigger;
-    // l = lean == "none" ? style.bigger : style.smaller;
-    // r = lean == "none" ? style.bigger : style.bigger;
-
     return (
-        <div className={style.card}>
-            <div className={`${style.card__left} ${l}`}>{left}</div>
-            <div className={`${style.card__right} ${r}`}>{right}</div>
+        <div className={style.row}>
+            <div className={`${style.card} ${style.card__left} ${l}`}>{left}</div>
+            <div className={`${style.card} ${style.card__right} ${r}`}>{right}</div>
         </div>
     );
 }
