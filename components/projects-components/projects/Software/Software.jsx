02eb056cebebd0e4a386494d9952f7ff1ba387f1
mairacanal <maira.canal@usp.br>
2021-03-12 19:36:12 -0300

    software: adding website to the Software projects tree

--- components/projects-components/projects/Software/Software.jsx
+++ components/projects-components/projects/Software/Software.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Model from './Model';
+import ProjectHeader from '../../ProjectHeader';
 import styles from 'styles/Projects.module.css';
 
 export default function Software(props) {
-  const { reportMaker } = props.textContent.projects;
-  const { zenithMonitor } = props.textContent.projects;
-  const { zenView } = props.textContent.projects;
+  const { reportMaker, zenithMonitor, zenView, website } = props.textContent.projects;
 
   return (
     <section className={`-section ${styles.projectsSection}`} style={{backgroundColor: '#000000'}}>
+      <ProjectHeader textContent ={ props.textContent } h1Color={ '#CCCCCC' } pColor={ '#CCCCCC' } />
       <Model textContent={ reportMaker } path={'images/Projetos/Software/ReportMaker.webp'} />
       <Model textContent={ zenithMonitor } path={'images/Projetos/Software/ZenithMonitor.webp'} />
       <Model textContent={ zenView } path={'images/Projetos/Software/ReportMaker.webp'} /> 
+      <Model textContent={ website } path={'images/Projetos/Software/ReportMaker.webp'} /> 
     </section>
   );
 }
