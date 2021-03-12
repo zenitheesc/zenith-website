b8d2491fca7bcb573311190964dfde21ad5d50b5
Matheus Vieira <matheus.v4200@gmail.com>
2021-03-12 20:47:47 -0300

    sectors-section: inicio do card dos setores

--- components/home-components/sectors-section/SectorCard.jsx
+++ components/home-components/sectors-section/SectorCard.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import style from 'styles/SectorsCards.module.css'
 
-export default function SectorCard() {
-  return <></>;
+export default function SectorCard({ textContent }) {
+  const { cores } = textContent;
+  return (
+    <div>
+      <div>
+        <h1>{textContent.title}</h1>
+        <span>{textContent.detail}</span>
+        <h2>{textContent.core}</h2>
+        <div>
+          {
+            cores.map((value, key) => {
+              return (
+                <div key={key}>
+                  <strong>{value.title}</strong>
+                  <spam>{value.detail}</spam>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
 }
