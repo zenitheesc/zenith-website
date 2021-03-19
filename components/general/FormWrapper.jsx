9849290ddb675bc15557925d7d3c6d5c4e73b08d
Math-42 <matheus.v4200@gmail.com>
2021-03-19 18:19:09 -0300

    test/cards: Ajustando css dos formulários

--- components/general/FormWrapper.jsx
+++ components/general/FormWrapper.jsx
@@ -4,8 +4,8 @@ import styles from 'styles/FormWrapper.module.css'
 export default function FormWrapper(props) {
 	const { link } = props;
 	return (
-		<div className={styles.formWrapper}>
-			<iframe src={link}>
+		<div className={[styles.formWrapper,"-section"].join(" ")}>
+			<iframe  className={"-standardContainer"} src={link}>
 				Loading...
 			</iframe>
 		</div>
