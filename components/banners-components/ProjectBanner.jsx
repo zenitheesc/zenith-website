e5ac455006f9bedaec2dec838aa6adc1b2424d26
mairacanal <maira.canal@usp.br>
2021-03-27 14:49:41 -0300

    videos: Genus new videos

--- components/banners-components/ProjectBanner.jsx
+++ components/banners-components/ProjectBanner.jsx
@@ -25,19 +25,23 @@ function ProjectBanner(props) {
 	return (
 		<section style={sectionStyle} className={`-section`}> 
 			<div className={`-standardContainer ${styles.projectBannerContainer}`}>
-				{videoAutoPlayable ? 
-					<video className={styles.projectBannerMidia} ref={videoElement} autoPlay loop={loop} muted playsInline preload="auto">
-						<source src={`/images/HomePage/banners/${folderName}/video.mp4`} type="video/mp4" />
-						<source src={`/images/HomePage/banners/${folderName}/video.webm`} type="video/webm" />
-						{/* <source src={`/images/HomePage/banners/${folderName}/video.mv4`} type="video/mp4" /> */}
-					</video>
-					: 
-					<picture>
-						<source srcset={`/images/HomePage/banners/${folderName}/static.webp`} type="image/webp" />
-						{/* <img src={`/images/HomePage/banners/${folderName}/static.png`} type="image/png" className={styles.projectBannerMidia}/> */}
-						<img src={`/images/HomePage/banners/${folderName}/static.webp`} type="image/webp" className={styles.projectBannerMidia}/>
-					</picture>
-				}
+				<ReactVisibilitySensor
+					active={!isVisible}
+					onChange={playVideo}>
+					{videoAutoPlayable ?
+						<video className={styles.projectBannerMidia} ref={videoElement} loop={loop} muted playsInline preload="auto">
+							<source src={`/images/HomePage/banners/${folderName}/videoIOS.mp4`} type="video/mp4" />
+							<source src={`/images/HomePage/banners/${folderName}/video.webm`} type="video/webm" />
+							<source src={`/images/HomePage/banners/${folderName}/video.mp4`} type="video/mp4" />
+						</video>
+						:
+						<picture>
+							<source srcset={`/images/HomePage/banners/${folderName}/static.webp`} type="image/webp" />
+							{/* <img src={`/images/HomePage/banners/${folderName}/static.png`} type="image/png" className={styles.projectBannerMidia}/> */}
+							<img src={`/images/HomePage/banners/${folderName}/static.webp`} type="image/webp" className={styles.projectBannerMidia} />
+						</picture>
+					}
+				</ReactVisibilitySensor>
 				<BannersTextSec textContent={textContent} />
 			</div>
 		</section>
