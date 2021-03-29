e2556b057ac8d9e1ccf3520d19eecafd35d0fe05
Leonardo Celente <leocelente@gmail.com>
2021-03-29 18:01:42 -0300

    Mininal Setup pra biblioteca AOS

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'styles/global.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    useEffect(() => {
+        AOS.init();
+      }, []);
+    
+    return <Component {...pageProps} />;
 }
