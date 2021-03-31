c06f8332e2e590eb2b3a247012bccaf6144226e7
mairacanal <maira.canal@usp.br>
2021-03-31 09:27:00 -0300

    clean: deleting empty files

--- components/general/Row1x1.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Row1x1() {
-  return <></>;
-}
--- components/general/Row4x6.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Row4x6() {
-  return <></>;
-}
