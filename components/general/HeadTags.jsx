225c8a824db243b6b2c241a7f05f3c52bf1026f4
mairacanal <maira.canal@usp.br>
2021-04-02 15:18:52 -0300

    analytics: adding Google Analytics

--- components/general/HeadTags.jsx
+++ components/general/HeadTags.jsx
@@ -18,6 +18,17 @@ export default function HeadTags(props) {
 				<link rel="alternate" href={`https://zenith.eesc.usp.br/es/${pageName}`} hrefLang="es" />
 				<link rel="alternate" href={`https://zenith.eesc.usp.br/en/${pageName}`} hrefLang="en" />
 				<link rel="alternate" href={`https://zenith.eesc.usp.br/${pageName}`} hrefLang="pt" />
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-TYP8Y3JHR0"></script>
+				<script
+					async
+					dangerouslySetInnerHTML={{
+					__html: `window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					
+					gtag('config', 'G-TYP8Y3JHR0');`
+					}}
+				/>
 			</Head>
 			<NextSeo
 				title={title}
