07fa9b22667fc828b001587b20136dd2f2cfe834
Fulvio <fulvio.f@usp.br>
2022-12-26 17:05:28 -0300

    launches: adds launches and optimizes the function that displays the menu.

--- components/launches-components/LaunchMenu.jsx
+++ components/launches-components/LaunchMenu.jsx
@@ -6,17 +6,14 @@ import LaunchItem from './LaunchItem';
 function LaunchMenu (props) {
     const { t } = useTranslation();
     const launches = t("lancamentos:launches", { count: -1 }, { returnObjects: true });
-    const sabado_4 = launches["sabado_4"];
-    const sabado_3 = launches["sabado_3"];
-    const sabado_2 = launches["sabado_2"];
-    const sabado_1 = launches["sabado_1"];
+    const rows = [];
+    for (let i = 0; i < 10; i++) {
+        rows.push(<LaunchItem className={styles.launchItem} textContent={launches[""+i]}></LaunchItem>);
+    }
 
     return (  
         <div className={styles.launchContainer}>
-            <LaunchItem className={styles.launchItem} textContent={sabado_4}></LaunchItem>
-            <LaunchItem className={styles.launchItem} textContent={sabado_3}></LaunchItem>
-            <LaunchItem className={styles.launchItem} textContent={sabado_2}></LaunchItem>
-            <LaunchItem className={styles.launchItem} textContent={sabado_1}></LaunchItem>
+            {rows}
         </div>      
     );
 }
