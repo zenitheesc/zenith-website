39a133b2d45b470a9cf1cebe4beee57d4b269a57
itMatos <italomatos764@gmail.com>
2026-05-21 22:56:07 -0300

    feat: refactor Map component and add MapProps type definition

--- pages/launches/index.tsx
+++ pages/launches/index.tsx
@@ -3,10 +3,5 @@ import { useMemo } from 'react';
 
 export default function LaunchesPage() {
   const Map = useMemo(() => dynamic(() => import('../../src/components/Map'), { ssr: false }), []);
-  return (
-    <div>
-      teste
-      <Map />
-    </div>
-  );
+  return <Map />;
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,26 +3,24 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
-
-type MapProps = {
-  position?: [number, number];
-  zoom?: number;
-};
+import { MapProps } from '../types/map.types';
 
 export default function MyMap(props: MapProps) {
   const { position = [0, 0], zoom = 2 } = props;
 
   return (
-    <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} style={{ height: '480px', width: '100%' }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
